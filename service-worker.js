22b32dabdd46d3f0a61fddc35732bf9d0f6495af
Maksim Eltyshev <meltyshev@gmail.com>
2020-11-15 20:18:54 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hodgepodge/precache-manifest.0b1196bf15127b3bfe6f9fb545617f0b.js"
+  "/hodgepodge/precache-manifest.0262e935ebd9c689e3370753dba2afb6.js"
 );
 
 self.addEventListener('message', (event) => {
